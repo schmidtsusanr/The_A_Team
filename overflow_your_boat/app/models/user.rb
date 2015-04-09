debf92c3d2f23ac925dd0f7d1f22931552c9ca5f
Durkheim & Morgan McCrory & Susan Schmidt <pair+durkheim.morganmccrory.schmidtsusanr@devbootcamp.com>
2015-04-09 18:57:26 -0500

    add user/session/welcome controllers, session helper, updated user model, added flash html to layout, routes, and the following views - sessions: new; users: create, edit, index, new, show, update; welcomes: index

--- overflow_your_boat/app/models/user.rb
+++ overflow_your_boat/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  has_many :questions
+  has_secure_password
 
+  has_many :questions
 
   has_many :answers_provided, class_name: "Answer", foreign_key: :answerer_id
   has_many :comments_provided, class_name: "Comment", foreign_key: :commenter_id
